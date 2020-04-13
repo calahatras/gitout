ce35f1ee72a471d47771626ee4fd44973ef6c28b
Michael <micke.default@gmail.com>
2020-04-13 12:42:42 +0200

    feature(git-tree): adds new converters used for git tree

---
+++ GitOut/Features/Git/Log/Converters/TreeToMarginConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace GitOut.Features.Git.Log.Converters
+{
+    public class TreeToMarginConverter : IValueConverter
+    {
+        public const int Distance = 20;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is GitTreeEvent gitTreeEvent))
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            int maxIndex = gitTreeEvent.Nodes
+                .Select(n => new [] { n.Top?.Up ?? 0, n.Top?.Down ?? 0, n.Bottom?.Down ?? 0})
+                .SelectMany(layers => layers)
+                .Max();
+
+            return new Thickness((maxIndex+1) * Distance, 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
