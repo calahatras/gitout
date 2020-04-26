5aaf6c6ffb903583822cee7baced57abefa43fea
Patrick Persson <calahatras@gmail.com>
2020-04-26 15:52:41 +0200

    refactor(stage): use style for vertical divider used in toolbar

--- GitOut/Features/Git/Stage/GitStageViewModel.cs
+++ GitOut/Features/Git/Stage/GitStageViewModel.cs
@@ -78,7 +78,9 @@ namespace GitOut.Features.Git.Stage
                 {
                     if (selectedChange != null)
                     {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                         ExecuteDiffAsync();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                     }
                 }
             }
@@ -110,7 +112,9 @@ namespace GitOut.Features.Git.Stage
                     SelectedDiff = null;
                     if (selectedChange != null)
                     {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                         ExecuteDiffAsync();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                     }
                 }
             }
