38e736ccab3df0b23246cac23f03b2c49057cf2a
Patrick Persson <calahatras@gmail.com>
2020-11-29 21:49:52 +0100

    clean(DiffDisplayOptions): format line breaks in constructor

--- GitOut/Features/Git/Stage/DiffDisplayOptions.cs
+++ GitOut/Features/Git/Stage/DiffDisplayOptions.cs
@@ -4,7 +4,12 @@ namespace GitOut.Features.Git.Stage
 {
     public class DiffDisplayOptions
     {
-        public DiffDisplayOptions(double pixelsPerDip, Brush dividerBrush, Brush headerForeground, ITextTransform transform)
+        public DiffDisplayOptions(
+            double pixelsPerDip,
+            Brush dividerBrush,
+            Brush headerForeground,
+            ITextTransform transform
+        )
         {
             PixelsPerDip = pixelsPerDip;
             DividerBrush = dividerBrush;
@@ -12,7 +17,16 @@ namespace GitOut.Features.Git.Stage
             Transform = transform;
         }
 
-        public DiffDisplayOptions(double pixelsPerDip, Brush dividerBrush, Brush headerForeground) : this(pixelsPerDip, dividerBrush, headerForeground, new PassThroughTransform()) { }
+        public DiffDisplayOptions(
+            double pixelsPerDip,
+            Brush dividerBrush,
+            Brush headerForeground
+        ) : this(
+            pixelsPerDip,
+            dividerBrush,
+            headerForeground,
+            new PassThroughTransform()
+        ) { }
 
         public double PixelsPerDip { get; }
         public Brush DividerBrush { get; }
