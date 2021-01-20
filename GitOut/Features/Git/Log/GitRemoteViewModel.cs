368f101be211e6efae118b1807fe226e482e24aa
Patrick Persson <calahatras@gmail.com>
2021-01-20 21:45:36 +0100

    feature(remote): show remotes in popup and allow user to fetch from selected remotes

---
+++ GitOut/Features/Git/Log/GitRemoteViewModel.cs
@@ -0,0 +1,40 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GitOut.Features.Git.Log
+{
+    public class GitRemoteViewModel : INotifyPropertyChanged
+    {
+        private bool isSelected;
+
+        public GitRemoteViewModel(GitRemote model, bool isSelected)
+        {
+            Name = model.Name;
+            Model = model;
+            this.isSelected = isSelected;
+        }
+
+        public string Name { get; }
+        public bool IsSelected
+        {
+            get => isSelected;
+            set => SetProperty(ref isSelected, value);
+        }
+        public GitRemote Model { get; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public static GitRemoteViewModel From(GitRemote model) => new GitRemoteViewModel(model, isSelected: true);
+
+        private bool SetProperty<T>(ref T prop, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (!ReferenceEquals(prop, value))
+            {
+                prop = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+            return false;
+        }
+    }
+}
