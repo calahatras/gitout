ff449ea217130f22d1e6e26fe938d19134670b07
Patrick Persson <calahatras@gmail.com>
2021-02-21 00:37:18 +0100

    enhancement(repositories): add toolbar and options for adding and removing repository in repository page

--- GitOut/Features/Git/RepositoryList/RepositoryListViewModel.cs
+++ GitOut/Features/Git/RepositoryList/RepositoryListViewModel.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Forms;
 using System.Windows.Input;
 using GitOut.Features.Collections;
 using GitOut.Features.Git.Log;
 using GitOut.Features.Git.Storage;
+using GitOut.Features.IO;
 using GitOut.Features.Navigation;
+using GitOut.Features.Wpf;
 
 namespace GitOut.Features.Git.RepositoryList
 {
@@ -16,7 +19,8 @@ namespace GitOut.Features.Git.RepositoryList
 
         public RepositoryListViewModel(
             INavigationService navigation,
-            IGitRepositoryStorage storage
+            IGitRepositoryStorage storage,
+            IGitRepositoryFactory repositoryFactory
         )
         {
             NavigateToLogCommand = new NavigateLocalCommand<IGitRepository>(
@@ -25,6 +29,17 @@ namespace GitOut.Features.Git.RepositoryList
                 repository => GitLogPageOptions.OpenRepository(repository),
                 repository => repository != null
             );
+
+            AddRepositoryCommand = new CallbackCommand(() =>
+            {
+                var dialog = new FolderBrowserDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    storage.Add(repositoryFactory.Create(DirectoryPath.Create(dialog.SelectedPath)));
+                }
+            });
+
+            RemoveRepositoryCommand = new CallbackCommand<IGitRepository>(repository => storage.Remove(repository));
 
             subscription = storage.Repositories.Subscribe(finalList =>
             {
@@ -43,6 +58,8 @@ namespace GitOut.Features.Git.RepositoryList
         }
 
         public ICommand NavigateToLogCommand { get; }
+        public ICommand AddRepositoryCommand { get; }
+        public ICommand RemoveRepositoryCommand { get; }
         public IEnumerable<IGitRepository> Repositories => repositories;
 
         public void Navigated(NavigationType type)
