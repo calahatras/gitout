665bab97e021ba46cb39d182f3e5352cf472f0e3
Patrick Persson <calahatras@gmail.com>
2021-03-21 13:09:28 +0100

    enhancement(text): add syntax highlight for line comments in csharp code

--- GitOut/Features/Text/CSharpSyntaxHighlighterOptions.cs
+++ GitOut/Features/Text/CSharpSyntaxHighlighterOptions.cs
@@ -4,6 +4,7 @@ namespace GitOut.Features.Text
 {
     public static class CSharpSyntaxHighlighterOptions
     {
+        public static readonly Brush CommentForegroundColor = new SolidColorBrush(Color.FromRgb(86, 214, 156));
         public static readonly Brush KeywordForegroundColor = new SolidColorBrush(Color.FromRgb(86, 156, 214));
         public static readonly Brush ControlKeywordForegroundColor = new SolidColorBrush(Color.FromRgb(216, 160, 223));
         public static readonly Brush StringForegroundColor = Brushes.PeachPuff;
