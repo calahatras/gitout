39be3fd2600a7beeea06bfbcf2b61c4119717e23
Michael <micke.default@gmail.com>
2021-05-19 22:18:58 +0200

    feature(log): ensure scrolled to item is in view

---
+++ GitOut/Features/Wpf/Scrolling/ScrollBehavior.cs
@@ -0,0 +1,27 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GitOut.Features.Wpf.Scrolling
+{
+    public static class ScrollBehavior
+    {
+        public static readonly DependencyProperty EnsureInViewProperty = DependencyProperty.RegisterAttached(
+            "EnsureInView",
+            typeof(object),
+            typeof(ScrollBehavior),
+            new PropertyMetadata(OnEnsureInViewChanged)
+        );
+
+        public static object GetEnsureInView(DependencyObject obj) => obj.GetValue(EnsureInViewProperty);
+
+        public static void SetEnsureInView(DependencyObject obj, object value) => obj.SetValue(EnsureInViewProperty, value);
+
+        private static void OnEnsureInViewChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ListBox listBox && e.NewValue is not null)
+            {
+                listBox.ScrollIntoView(e.NewValue);
+            }
+        }
+    }
+}
