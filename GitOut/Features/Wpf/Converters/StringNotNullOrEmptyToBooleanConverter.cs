cc71018e036a69d16cfe9fce6de55ab6a821a451
Michael <micke.default@gmail.com>
2021-11-11 22:18:48 +0100

    enhancement(log): adds contextmenu to git history details

---
+++ GitOut/Features/Wpf/Converters/StringNotNullOrEmptyToBooleanConverter.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace GitOut.Features.Wpf.Converters
+{
+    [ValueConversion(typeof(string), typeof(bool))]
+    public class StringNotNullOrEmptyToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is not string str || !string.IsNullOrEmpty(str);
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => Binding.DoNothing;
+    }
+}
