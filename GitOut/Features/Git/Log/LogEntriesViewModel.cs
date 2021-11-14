171d4b02ce31cc3d700c6acb801ed751ae688aab
Michael <micke.default@gmail.com>
2021-11-14 20:40:15 +0100

    refactor(logentriesviewmodel): add line breaks for parameters

--- GitOut/Features/Git/Log/LogEntriesViewModel.cs
+++ GitOut/Features/Git/Log/LogEntriesViewModel.cs
@@ -37,21 +37,24 @@ namespace GitOut.Features.Git.Log
             Root = root;
             this.repository = repository;
 
-            allFiles = new SortedLazyAsyncCollection<IGitFileEntryViewModel, RelativeDirectoryPath>(_ => ListAllFilesAsync(), IGitDirectoryEntryViewModel.CompareItems);
-            var logFiles = new SortedLazyAsyncCollection<IGitFileEntryViewModel, RelativeDirectoryPath>(relativePath =>
-                GitFileEntryViewModelFactory.ListIdAsync(root.Event.Id, repository, relativePath),
+            allFiles = new SortedLazyAsyncCollection<IGitFileEntryViewModel, RelativeDirectoryPath>(
+                _ => ListAllFilesAsync(),
+                IGitDirectoryEntryViewModel.CompareItems
+            );
+            var logFiles = new SortedLazyAsyncCollection<IGitFileEntryViewModel, RelativeDirectoryPath>(
+                relativePath => GitFileEntryViewModelFactory.ListIdAsync(root.Event.Id, repository, relativePath),
                 IGitDirectoryEntryViewModel.CompareItems
             );
 
             _ = logFiles.MaterializeAsync(RelativeDirectoryPath.Root).AsTask();
             this.logFiles = logFiles;
-            diffFiles = new SortedLazyAsyncCollection<IGitFileEntryViewModel, RelativeDirectoryPath>(relativePath =>
-                GitFileEntryViewModelFactory.DiffIdAsync(diff?.Event.Id ?? root.Event.Parent?.Id, root.Event.Id, repository, RelativeDirectoryPath.Root),
+            diffFiles = new SortedLazyAsyncCollection<IGitFileEntryViewModel, RelativeDirectoryPath>(
+                relativePath => GitFileEntryViewModelFactory.DiffIdAsync(diff?.Event.Id ?? root.Event.Parent?.Id, root.Event.Id, repository, RelativeDirectoryPath.Root),
                 IGitDirectoryEntryViewModel.CompareItems
             );
 
-            flattenedDiffFiles = new SortedLazyAsyncCollection<IGitFileEntryViewModel, RelativeDirectoryPath>(relativePath =>
-                GitFileEntryViewModelFactory.DiffAllAsync(diff?.Event.Id ?? root.Event.Parent?.Id, root.Event.Id, repository),
+            flattenedDiffFiles = new SortedLazyAsyncCollection<IGitFileEntryViewModel, RelativeDirectoryPath>(
+                relativePath => GitFileEntryViewModelFactory.DiffAllAsync(diff?.Event.Id ?? root.Event.Parent?.Id, root.Event.Id, repository),
                 IGitDirectoryEntryViewModel.CompareItems
             );
 
