4d1813b251e0d0656b2422a831134d47eabf5b81
Patrick Persson <calahatras@gmail.com>
2026-03-02 23:00:30 +0100

    wip(modal): try out window chrome set to none for cherry pick dialog

--- GitOut/Features/Git/Log/CherryPickOptionsPage.xaml.cs
+++ GitOut/Features/Git/Log/CherryPickOptionsPage.xaml.cs
@@ -8,5 +8,21 @@ public partial class CherryPickOptionsPage : UserControl
     {
         InitializeComponent();
         DataContext = viewModel;
+
+        Loaded += (s, e) =>
+        {
+            var window = System.Windows.Window.GetWindow(this);
+            if (window != null)
+            {
+                window.Background = System.Windows.Media.Brushes.Transparent;
+                System.Windows.Shell.WindowChrome.SetWindowChrome(window, new System.Windows.Shell.WindowChrome
+                {
+                    CaptionHeight = 0,
+                    CornerRadius = new System.Windows.CornerRadius(8),
+                    GlassFrameThickness = new System.Windows.Thickness(-1),
+                    ResizeBorderThickness = new System.Windows.Thickness(0)
+                });
+            }
+        };
     }
 }
